18aad6c8810a8b4cbaed9609bcaba76ad181efee
Võ Văn Mười <67988045+Tenaity@users.noreply.github.com>
2025-09-23 11:21:28 +0700

    fix(frontend): mock canvas context in tests

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts'
+    setupFiles: ['./vitest.setup.ts', 'src/test/setup.ts'],
+    restoreMocks: true
   }
 })
