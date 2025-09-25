bfbea41e4956ba387ed47741cb68d312cb8ccbb2
Tenaity <tenaiti.150320@gmail.com>
2025-09-25 20:30:03 +0700

    chore(ci): enforce case-sensitive imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,24 @@ module.exports = {
   env: { browser: true, es2021: true },
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module", ecmaFeatures: { jsx: true } },
-  plugins: ["@typescript-eslint", "react", "jsx-a11y", "react-hooks"],
+  plugins: ["@typescript-eslint", "react", "jsx-a11y", "react-hooks", "import"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
     "prettier",
   ],
-  settings: { react: { version: "detect" } },
+  settings: {
+    react: { version: "detect" },
+    "import/resolver": {
+      typescript: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
   ignorePatterns: ["dist", "build", ".vite", "coverage"],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
@@ -22,5 +30,7 @@ module.exports = {
     "jsx-a11y/label-has-associated-control": "off",
     "jsx-a11y/click-events-have-key-events": "off",
     "jsx-a11y/no-static-element-interactions": "off",
+    "import/named": "off",
+    "import/no-unresolved": ["error", { caseSensitive: true }],
   },
 };
